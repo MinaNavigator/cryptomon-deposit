0b9346e990fd234cc7fc09090efb3ce1cd1f0c5d
Youtpout <EddyBoughioul@hotmail.fr>
2024-04-24 09:40:58 +0200

    remove unused contract

--- contracts/src/index.ts
+++ contracts/src/index.ts
@@ -1,6 +1,3 @@
-import { Add } from './Add.js';
-import { GameDeposit, DepositData } from './gamedeposit.js';
-import { GameContract, WithdrawData } from './gamecontract.js';
 import { GameManager } from './gamemanager.js';
 
-export { Add, GameDeposit, DepositData, GameContract, WithdrawData, GameManager };
+export { GameManager };
