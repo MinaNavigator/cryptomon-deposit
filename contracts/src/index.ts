d71d09af8e7dc8f03b4beaaaf119b23991536a74
Youtpout <EddyBoughioul@hotmail.fr>
2024-04-24 09:39:27 +0200

    create script to deploy vk

--- contracts/src/index.ts
+++ contracts/src/index.ts
@@ -1,6 +1,6 @@
 import { Add } from './Add.js';
 import { GameDeposit, DepositData } from './gamedeposit.js';
 import { GameContract, WithdrawData } from './gamecontract.js';
-import { GameManager } from './gamemanager';
+import { GameManager } from './gamemanager.js';
 
 export { Add, GameDeposit, DepositData, GameContract, WithdrawData, GameManager };
